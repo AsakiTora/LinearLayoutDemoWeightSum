378263d29ca987bca1e227133b26849462df433b
Asaki Tora <78132550+AsakiTora@users.noreply.github.com>
2021-06-24 09:40:48 +0700

    first commit

---
+++ app/src/main/java/fpoly/andoid/linearlayout2/NoneWeightSum.java
@@ -0,0 +1,14 @@
+package fpoly.andoid.linearlayout2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NoneWeightSum extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_none_weight_sum);
+    }
+}
---
+++ app/src/main/java/fpoly/andoid/linearlayout2/WeightSum.java
@@ -0,0 +1,14 @@
+package fpoly.andoid.linearlayout2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WeightSum extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weight_sum);
+    }
+}
